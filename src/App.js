c7390a977958347cd99334b3d6c962bf8a767ed2
Hakan808 <hakan.alptekin03@gmail.com>
2025-08-19 16:14:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
+
+const projects = [
+  {
+    name: "Graph API",
+    initials: "GA",
+    href: "#",
+    members: 16,
+    bgColor: "bg-pink-600",
+    isPinned: true,
+  },
+  {
+    name: "Component Design",
+    initials: "CD",
+    href: "#",
+    members: 12,
+    bgColor: "bg-purple-600",
+    isPinned: true,
+  },
+  {
+    name: "Templates",
+    initials: "T",
+    href: "#",
+    members: 16,
+    bgColor: "bg-yellow-500",
+    isPinned: false,
+  },
+  {
+    name: "React Components",
+    initials: "RC",
+    href: "#",
+    members: 8,
+    bgColor: "bg-green-500",
+    isPinned: true,
+  },
+];
+
+// Pinlenmiş (isPinned) projeleri görüntüleyen liste oluşturacağız.
+// Verilen projects array'inde, yalnızca isPinned özelliği true olan projeleri görüntülemek için filter ve map yöntemlerini kullanın.
+// Listeyi PinnedProjects bileşeni aracılığıyla oluşturun ve project bilgilerini dinamik olarak render edin.
+// Çıktı örneği için public/preview.png dosyasını inceleyebilirsiniz.
+
+// Not: EllipsisVerticalIcon bileşeni, Tailwind Heroicons kütüphanesinden alınmış ikon bileşenidir.
+// Dikey üç nokta simgesi (vertical ellipsis) olarak görünür ve genellikle menü veya daha fazla işlem seçeneklerini göstermek için kullanılır.
+// Daha fazla bilgi için: https://heroicons.com/
+
+export default function PinnedProjects() {
+  return (
+    <div className="projects-wrapper p-5">
+      <h2 className="projects-title py-5 text-center text-xl font-bold text-emerald-800">Pinlenmiş Projeler</h2>
+      <ul className="projects-list ">
+        {projects
+          .filter((project) => project.isPinned)
+          .map((item) => {
+            const {name,initials,href,members,bgColor} = item;
+            return (
+              <li className="project-list-item">
+                <div className={` project-initials ${bgColor} p-5 text-white font-bold rounded-sm`}>{initials} </div>
+                <div className="project-content-wrapper">
+                  <div className="project-content">
+                    <a href="#" className="project-link font-bold text-emerald-800 hover:underline">
+                      {name}
+                    </a>
+                    <p className="project-members font-light text-emerald-800">{members} üyeler</p>
+                  </div>
+                  <div className="project-button-wrapper">
+                    <button type="button" className="project-button">
+                      <EllipsisVerticalIcon className="project-icon" />
+                    </button>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
